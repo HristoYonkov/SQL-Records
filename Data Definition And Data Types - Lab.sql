4c43eb0e68fadb89bce131466ec7dd6e7b7151c6
Hristo Yonkov <hristopturs@gmail.com>
2023-05-18 10:45:30 +0300

    Create Data Definition And Data Types - Lab.sql

---
+++ Data Definition And Data Types - Lab.sql
@@ -0,0 +1,25 @@
+-- 01. Create Tables
+CREATE TABLE employees (
+	id INT primary key AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE categories (
+	id INT primary key  AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE products (
+	id INT primary key  AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+	category_id VARCHAR(50) NOT NULL
+);
+
+-- 02. Insert Data in Tables
+INSERT INTO employees
+VALUES (1, "Stamat", "Stamatov"), (2, "Pavel", "Dimitrov"), (3, "Peter", "Petrov");
+
+-- 03. Alter Tables
+ALTER TABLE employees
+ADD COLUMN middle_name VARCHAR(50) NOT NULL;
