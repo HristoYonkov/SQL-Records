0b49345bf4b87b856ae2681f47c941a51dfcf7aa
Hristo Yonkov <hristopturs@gmail.com>
2023-05-18 11:43:49 +0300

    Create Exercise-Data Definition and Datatypes.sql

---
+++ Exercise-Data Definition and Datatypes.sql
@@ -0,0 +1,22 @@
+-- 1. Create Tables
+CREATE TABLE minions (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    age INT
+);
+
+CREATE TABLE towns (
+    town_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+);
+
+-- 2. Alter Minions Table
+
+
+-- 3. Insert Records in Both Tables
+
+
+-- 4. Truncate Table Minions
+
+
+-- 5. Drop All Tables
