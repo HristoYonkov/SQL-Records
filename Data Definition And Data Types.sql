44202d9958a6165e44c5365d2d08123064e737c0
Hristo Yonkov <hristopturs@gmail.com>
2023-05-18 11:43:41 +0300

    Create Data Definition And Data Types.sql

---
+++ Data Definition And Data Types.sql
@@ -0,0 +1,34 @@
+-- 01. Create Tables
+CREATE TABLE employees (
+	id INT primary key AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE categories (
+	id INT primary key  AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE products (
+	id INT primary key  AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+	category_id VARCHAR(50) NOT NULL
+);
+
+-- 02. Insert Data in Tables
+INSERT INTO employees
+VALUES (1, "Stamat", "Stamatov"), (2, "Pavel", "Dimitrov"), (3, "Peter", "Petrov");
+
+-- 03. Alter Tables
+ALTER TABLE employees
+ADD COLUMN middle_name VARCHAR(50) NOT NULL;
+
+-- 04. Adding Constraints
+ALTER TABLE products
+ADD CONSTRAINT fk_categories_id 
+FOREIGN KEY(category_id) REFERENCES categories(id);
+
+-- 05. Modifying Columns
+ALTER TABLE employees
+MODIFY COLUMN middle_name VARCHAR(100);
