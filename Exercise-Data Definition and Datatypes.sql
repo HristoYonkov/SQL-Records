c4a687af7aeddce7dce64b8404510674fc8a7703
Hristo Yonkov <hristopturs@gmail.com>
2023-05-18 17:57:35 +0300

    Update Exercise-Data Definition and Datatypes.sql

--- Exercise-Data Definition and Datatypes.sql
+++ Exercise-Data Definition and Datatypes.sql
@@ -20,7 +20,11 @@ ADD CONSTRAINT fk_minions_towns FOREIGN KEY minions(town_id)
 REFERENCES towns(id);
 
 -- 3. Insert Records in Both Tables
+INSERT INTO towns (id, name)
+VALUES(1, 'Sofia'), (2, 'Plovdiv'), (3, 'Varna');
 
+INSERT INTO minions (id, name, age, town_id)
+VALUES(1, 'Kevin', 22, 1), (2, 'Bob', 15, 3), (3, 'Steward', NULL, 2);
 
 -- 4. Truncate Table Minions
 
