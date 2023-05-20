93205cc756aebf339af5e1d01ed1a4e9d82dc137
Hristo Yonkov <hristopturs@gmail.com>
2023-05-20 11:34:16 +0300

    Update Exercise-Data Definition and Datatypes.sql

--- Exercise-Data Definition and Datatypes.sql
+++ Exercise-Data Definition and Datatypes.sql
@@ -149,7 +149,59 @@ VALUES ('Iron Man', 1, 2, 3),
 
 
 -- 13. Basic Insert
+CREATE TABLE towns (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30)
+);
 
+CREATE TABLE addresses (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    address_text VARCHAR(50),
+    town_id INT,
+    FOREIGN KEY (town_id)
+    REFERENCES towns(id)
+);
+
+CREATE TABLE departments (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30)
+);
+
+CREATE TABLE employees (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    middle_name VARCHAR(30),
+    last_name VARCHAR(30),
+    job_title VARCHAR(30),
+    department_id INT,
+    hire_date DATE,
+    salary DOUBLE,
+    address_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id),
+    FOREIGN KEY (address_id)
+    REFERENCES addresses(id)
+);
+--  Insert Into Judge this solution!
+INSERT INTO towns (name)
+VALUES ('Sofia'),
+('Plovdiv'),
+('Varna'),
+('Burgas');
+
+INSERT INTO departments (name)
+VALUES ('Engineering'),
+('Sales'),
+('Marketing'),
+('Software Development'),
+('Quality Assurance');
+
+INSERT INTO employees (first_name, middle_name, last_name, job_title, department_id, hire_date, salary)
+VALUES ('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '2013-02-01', 3500.00),
+('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '2004-03-02', 4000.00),
+('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '2016-08-28', 525.25),
+('Georgi', 'Terziev', 'Ivanov', 'CEO', 2, '2007-12-09', 3000.00),
+('Peter', 'Pan', 'Pan', 'Intern', 3, '2016-08-28', 599.88);
 
 -- 14. Basic Select All Fields
 SELECT * FROM towns;
