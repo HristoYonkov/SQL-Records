f35f920dc1870556af35abf9cec7792463f7b8f8
Hristo Yonkov <hristopturs@gmail.com>
2023-05-22 16:04:09 +0300

    Update 3.Basic CRUD.sql

--- 3.Basic CRUD.sql
+++ 3.Basic CRUD.sql
@@ -6,3 +6,10 @@ salary
 FROM employees ORDER BY id;
 
 -- 02. Select Employees with Filter
+SELECT  id,
+CONCAT_WS(' ', first_name, last_name) AS full_name,
+job_title,
+salary
+FROM employees WHERE salary > 1000;
+
+-- 03. Update Salary and Select
