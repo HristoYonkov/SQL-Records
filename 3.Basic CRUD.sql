9a9a9ace592a1b183dcee28545d851a88327ac58
Hristo Yonkov <hristopturs@gmail.com>
2023-05-23 14:12:02 +0300

    Update 3.Basic CRUD.sql

--- 3.Basic CRUD.sql
+++ 3.Basic CRUD.sql
@@ -19,3 +19,16 @@ SET salary = salary + 100
 WHERE job_title = 'Manager';
     
 SELECT salary FROM employees;
+
+-- 04. Top Paid Employee
+CREATE VIEW v_top_payd_employee AS
+SELECT * from employees
+ORDER BY salary DESC LIMIT 1;
+
+SELECT * FROM v_top_payd_employee;
+
+-- 05. Select Employees by Multiple Filters
+SELECT * FROM employees AS e
+WHERE e.department_id = 4 AND e.salary >= 1000;
+
+-- 06. Delete from Table
