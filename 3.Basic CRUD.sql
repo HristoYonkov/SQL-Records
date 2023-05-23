55397230335a60c2bb82e783d028f68bafc2deff
Hristo Yonkov <hristopturs@gmail.com>
2023-05-23 13:09:18 +0300

    Update 3.Basic CRUD.sql

--- 3.Basic CRUD.sql
+++ 3.Basic CRUD.sql
@@ -13,3 +13,9 @@ salary
 FROM employees WHERE salary > 1000;
 
 -- 03. Update Salary and Select
+SET SQL_SAFE_UPDATES = 0;
+UPDATE employees
+SET salary = salary + 100
+WHERE job_title = 'Manager';
+    
+SELECT salary FROM employees;
