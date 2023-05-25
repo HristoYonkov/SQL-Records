be81aba685a9b442e937a084d2708e371d830806
Hristo Yonkov <hristopturs@gmail.com>
2023-05-25 16:30:04 +0300

    Update 6.Exercise-Built-in Functions.sql

--- 6.Exercise-Built-in Functions.sql
+++ 6.Exercise-Built-in Functions.sql
@@ -1,3 +1,48 @@
+-- 01. Find Names of All Employees by First Name
 SELECT first_name, last_name FROM employees
 WHERE LOWER(first_name) LIKE 'sa%'
 ORDER BY employee_id;
+
+-- 02. Find Names of All Employees by Last Name
+SELECT first_name, last_name FROM employees
+WHERE LOWER(last_name) LIKE '%ei%'
+ORDER BY employee_id;
+
+-- 03. Find First Names of All Employess
+SELECT first_name FROM employees
+WHERE department_id IN(3, 10) AND
+YEAR(hire_date) BETWEEN 1995 AND 2005
+ORDER BY employee_id;
+
+-- 04. Find All Employees Except Engineers
+SELECT first_name, last_name FROM employees
+WHERE job_title NOT LIKE '%engineer%'
+ORDER BY employee_id;
+
+-- 05. Find Towns with Name Length
+SELECT name FROM towns
+WHERE CHAR_LENGTH(name) IN(5, 6)
+ORDER BY name;
+
+-- 06. Find Towns Starting With
+SELECT town_id, name FROM towns
+WHERE SUBSTRING(LOWER(name), 1, 1) IN('m', 'k', 'b', 'e')
+ORDER BY name;
+
+-- 07. Find Towns Not Starting With
+SELECT * FROM towns
+WHERE SUBSTRING(LOWER(name), 1, 1) NOT IN('r', 'b', 'd')
+ORDER BY name;
+-- second solution
+SELECT * FROM towns
+WHERE name REGEXP('^[^rRdRbB]')
+ORDER BY name;
+
+-- 08. Create View Employees Hired After
+CREATE VIEW v_employees_hired_after_2000 AS
+SELECT first_name, last_name FROM employees
+WHERE YEAR(hire_date) > 2000;
+
+-- 09. Length of Last Name
+SELECT first_name, last_name FROM employees
+WHERE CHAR_LENGTH(last_name) = 5;
