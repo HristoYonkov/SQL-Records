f8e55c01b8a6a875304240225241a1fab204ac1f
Hristo Yonkov <hristopturs@gmail.com>
2023-05-25 23:05:40 +0300

    Updated

--- 6.Exercise-Built-in Functions.sql
+++ 6.Exercise-Built-in Functions.sql
@@ -95,6 +95,21 @@ WHERE ip_address LIKE '___.1%.%.___'
 ORDER BY user_name;
 
 -- 15. Show All Games with Duration and Part of the Day
-
+SELECT name AS games,
+CASE 
+	WHEN HOUR(start) BETWEEN 0 AND 11 THEN 'Morning'
+	WHEN HOUR(start) BETWEEN 12 AND 17 THEN 'Afternoon'
+ELSE 'Evening' END AS 'Part of the Day',
+CASE
+	WHEN duration <= 3 THEN 'Extra Short'
+	WHEN duration BETWEEN 4 AND 6 THEN 'Short'
+	WHEN duration BETWEEN 7 AND 10 THEN 'Long'
+    ELSE 'Extra Long' END AS 'Duration'
+FROM games;
 
 -- 16. Orders Table
+SELECT product_name,
+order_date,
+DATE_ADD(order_date, INTERVAL 3 DAY) AS pay_due,
+DATE_ADD(order_date,  INTERVAL 1 MONTH) AS deliver_due
+FROM orders;
