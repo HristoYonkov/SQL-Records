a73149b9d081fba90388d0f0bb30a79788c5a3cd
Hristo Yonkov <hristopturs@gmail.com>
2023-05-26 12:07:55 +0300

    Update 5.Built-in Functions.sql

--- 5.Built-in Functions.sql
+++ 5.Built-in Functions.sql
@@ -19,14 +19,73 @@ SELECT title FROM books
 WHERE SUBSTRING(title, 1, 12) = 'Harry Potter'
 ORDER BY id;
 
------------------------------------------------------------------
 
+------------------------------------- String Functions-------------------------------
+SUBSTRING(String, Position, Length) -- extracts part of a string
+REPLACE(String, Pattern, Replacement) -- replaces specific string with another
+LTRIM(String) -- remove spaces from either side of string
+RTRIM(String) -- remove spaces from either side of string
+CHAR_LENGTH(String) -- count number of characters
+LENGTH(String) -- get number of used bytes (double for Unicode)
+LEFT(String, Count), RIGHT(String, Count) -- get characters from beginning or end of string
+LOWER(String), UPPER(String) -- change letter casing
+REVERSE(String) -- reverse order of all characters in string
+REPEAT(String, Count) -- repeat string
+LOCATE(Pattern, String, [Position]) -- locate specific pattern (substring) in string
+INSERT(String, Position, Length, Substring) -- Insert substring at specific position
+
+----------------------------- Arithmetical Operators and Numeric functions -----------
+-- Name Description
+'DIV'  -- Integer division
+'/' -- Division operator
+'-' -- Minus Operator
+'%, MOD' -- Modulo operator
+'+' -- Addition operator
+'*' -- Multiplication operator
+'- (arg)' -- Change sign of argument
+-- Numeric Functions
+PI() -- get the value of Pi (15 –digit precision)
+ABS(Value) -- absolute value
+SQRT(Value) -- square root
+POW(Value, Exponent) -- raise value to desired exponent
+CONV(Value, from_base, to_base) -- Converts numbers between different number bases
+ROUND(Value, Precision) -- obtain desired precision
+FLOOR(Value) -- return the nearest integer
+CEILING(Value) -- return the nearest integer
+SIGN(Value) -- returns +1, -1 or 0, depending on value sign
+RAND(Seed) -- get a random value in range [0,1). If Seed is not specified, one is assigned at random
+
+-----------------------------------------Wildcards -----------------------------------
+-- Used to substitute any other character(s) in a string
+'%' -- represents zero, one, or multiple characters
+'_' -- represents a single character
+-- Can be used in combinationsUsed with LIKE operator in a WHERE clause
+WHERE customer_name LIKE '_r%'; -- Find any values that have "r" in second position
+WHERE customer_name LIKE 'a%'; -- Find any values that start with "a"
+WHERE customer_name LIKE 'a%o'; -- Finds any values that starts with "a" and ends with "o"
+'\' -- specify prefix to treat special characters as normal
+[charlist] -- specifying which characters to look for
+[!charlist] -- excluding characters
+
+---------------------------------------Date Functions--------------------------------
+EXTRACT(Part FROM Date) -- extract a segment from a date as an integer
+TIMESTAMPDIFF(Part, FirstDate, SecondDate) -- find difference between two dates
+'year, %Y, %y', 'month, %M, %m', 'day, %w, %D' -- Part can be any part and format of date or time
+YEAR(Date), MONTH(Date), DAY(Date) -- Part can be any part and format of date or time
+
+DATE_FORMAT() -- formats the date value according to the format
+SELECT DATE_FORMAT('2017/05/31', '%Y %b %D') AS 'Date';
+
+NOW() -- NOW – obtain current date and time
+SELECT NOW();
+
+--------------------------------------------------------------------------------
 DATE_FORMAT(date, format)
 -- E.g.
 SELECT DATE_FORMAT(dateField, '%m/%d/%Y') FROM TableName;
 -- See https://www.mysqltutorial.org/mysql-date_format/ for available formats
 
-Format	Description
+-- Format Description
 %a	Abbreviated weekday name (Sun to Sat)
 %b	Abbreviated month name (Jan to Dec)
 %c	Numeric month name (0 to 12)
