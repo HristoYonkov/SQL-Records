76818a241243bcc662a8908563286cc33441db4d
Hristo Yonkov <hristopturs@gmail.com>
2023-05-26 17:37:55 +0300

    Update 4.1.Data Aggregation.sql

--- 4.1.Data Aggregation.sql
+++ 4.1.Data Aggregation.sql
@@ -1,7 +1,17 @@
-------------------------------------- Grouping ------------------------
---Grouping allows taking data into separate groups based on a common property
+------------------------------------- Grouping -------------------------------
+GROUP BY --Grouping allows taking data into separate groups based on a common property
 SELECT e.`job_title`, count(employee_id)
 FROM `employees` AS e
 GROUP BY e.`job_title`;
 
----------------------------------- Aggregate Functions ----------------
+---------------------------------- Aggregate Functions -----------------------
+'COUNT, SUM, MAX, MIN, AVG…' -- Used to operate over one or more groups -
+--performing data analysis on every one. They usually ignore NULL values.
+
+COUNT() -- counts the values (not nulls) in one or more columns -
+--based on grouping criteria
+
+SUM() -- sums the values in a column.
+MAX() -- takes the maximum value in a column.
+MIN() -- takes the minimum value in a column. 
+AVG() -- calculates the average value in a column. 
