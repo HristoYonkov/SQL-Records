46f441c4de916da39c26eb1c661ecb35177da19a
Hristo Yonkov <hristopturs@gmail.com>
2023-05-27 20:42:49 +0300

    Update 4.2.Exercise-Data Aggregation.sql

--- 4.2.Exercise-Data Aggregation.sql
+++ 4.2.Exercise-Data Aggregation.sql
@@ -61,3 +61,5 @@ SELECT deposit_group, is_deposit_expired, AVG(deposit_interest) AS average_inter
 WHERE deposit_start_date > '1985-01-01'
 GROUP BY deposit_group, is_deposit_expired
 ORDER BY deposit_group DESC, is_deposit_expired;
+
+-- 12. Employees Minimum Salaries
