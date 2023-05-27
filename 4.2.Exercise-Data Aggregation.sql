7fabd3b260b82f4bb2d1621474567af15349ce26
Hristo Yonkov <hristopturs@gmail.com>
2023-05-27 17:21:53 +0300

    Update 4.2.Exercise-Data Aggregation.sql

--- 4.2.Exercise-Data Aggregation.sql
+++ 4.2.Exercise-Data Aggregation.sql
@@ -3,3 +3,8 @@ SELECT COUNT(*) FROM wizzard_deposits;
 
 -- 02. Longest Magic Wand
 SELECT MAX(magic_wand_size) AS longest_magic_wand FROM wizzard_deposits;
+
+-- 03. Longest Magic Wand per Deposit Groups
+SELECT deposit_group AS deposit_group, MAX(magic_wand_size) AS magic_wand_size FROM wizzard_deposits
+GROUP BY deposit_group
+ORDER BY magic_wand_size, deposit_group;
