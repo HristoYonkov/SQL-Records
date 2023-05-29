9162fe2fb17033dd4c93808ef23a574cb2178f6a
Hristo Yonkov <hristopturs@gmail.com>
2023-05-29 16:14:49 +0300

    Update 5.2.Exercise-Table Relations.sql

--- 5.2.Exercise-Table Relations.sql
+++ 5.2.Exercise-Table Relations.sql
@@ -19,3 +19,20 @@ ALTER TABLE people
 ADD FOREIGN KEY(passport_id) REFERENCES passports (passport_id);
 
 -- 02. One-To-Many Relationship
+CREATE TABLE manufacturers (
+    manufacturer_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    established_on DATE NOT NULL
+);
+INSERT INTO manufacturers
+VALUES (1, 'BMW', '1916-03-01'), (2, 'Tesla', '2003-01-01'), (3, 'Lada', '1966-05-01');
+
+CREATE TABLE models (
+	model_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(20),
+    manufacturer_id INT,
+    FOREIGN KEY(manufacturer_id) REFERENCES manufacturers (manufacturer_id)
+);
+INSERT INTO models
+VALUES (101, 'X1', 1), (102, 'i6', 1), (103, 'Model S', 2),
+(104, 'Model X', 2), (105, 'Model 3', 2), (106, 'Nova', 3);
