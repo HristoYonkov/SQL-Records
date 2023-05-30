107183f1e5a1da206ceeec401c50a78e295244a9
Hristo Yonkov <hristopturs@gmail.com>
2023-05-30 15:36:34 +0300

    Update 6.1.Subqueries and JOINs.sql

--- 6.1.Subqueries and JOINs.sql
+++ 6.1.Subqueries and JOINs.sql
@@ -1,14 +1,26 @@
 -- 1.Managers
-SELECT 
-    departments.manager_id AS employee_id,
-    CONCAT(employees.first_name, ' ', employees.last_name) AS full_name,
-    departments.department_id,
-    name AS department_name
+SELECT departments.manager_id AS employee_id,
+CONCAT(employees.first_name, ' ', employees.last_name) AS full_name,
+departments.department_id,
+name AS department_name
 FROM
-    departments
-        INNER JOIN
-    employees
-    WHERE employees.employee_id = departments.manager_id
-    ORDER BY employee_id
-    LIMIT 5;
+departments
+    LEFT JOIN
+employees
+ON employees.employee_id = departments.manager_id
+ORDER BY employee_id
+LIMIT 5;
 
+-- 2. Towns and Addresses
+SELECT towns.town_id,
+name AS town_name,
+address_text 
+FROM towns
+JOIN addresses ON towns.town_id = addresses.town_id
+WHERE towns.name IN('San Francisco', 'Sofia', 'Carnation')
+ORDER BY town_id, address_id;
+
+-- 3. Employees Without Managers
+
+
+-- 4. High Salary
