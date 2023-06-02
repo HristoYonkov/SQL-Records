a0e3a36270a723994842208f74fee4e9fb9f4fae
Hristo Yonkov <hristopturs@gmail.com>
2023-06-02 11:48:51 +0300

    Update 7.1.Database Programmability and Transactions.sql

--- 7.1.Database Programmability and Transactions.sql
+++ 7.1.Database Programmability and Transactions.sql
@@ -84,3 +84,37 @@ SET salary = salary * 1.05
 WHERE d.name = department_name;
 END
 -----------------------------------------------------------------------------------------
+-- 3. Employees Promotion By ID
+CREATE PROCEDURE usp_raise_salary_by_id(id int)
+BEGIN
+START TRANSACTION;
+IF((SELECT count(employee_id) FROM employees WHERE employee_id
+like id)<>1) THEN
+ROLLBACK;
+ELSE
+UPDATE employees AS e SET salary = salary + salary*0.05
+WHERE e.employee_id = id;
+END IF;
+END
+
+-- 4. Triggered
+CREATE TABLE deleted_employees(
+employee_id INT PRIMARY KEY AUTO_INCREMENT,
+first_name VARCHAR(20),
+last_name VARCHAR(20),
+middle_name VARCHAR(20),
+job_title VARCHAR(50),
+department_id INT,
+salary DOUBLE
+);
+
+CREATE TRIGGER tr_deleted_employees
+AFTER DELETE
+ON employees
+FOR EACH ROW
+BEGIN
+INSERT INTO deleted_employees (first_name,last_name,
+middle_name,job_title,department_id,salary)
+VALUES(OLD.first_name,OLD.last_name,OLD.middle_name,
+OLD.job_title,OLD.department_id,OLD.salary);
+END;
