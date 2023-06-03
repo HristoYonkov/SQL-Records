c571998fe4fd33e2adc71bba9fdb9dc6836d3349
Hristo Yonkov <hristopturs@gmail.com>
2023-06-03 17:54:50 +0300

    Update 7.2.Exercise-Functions and Procedures.sql

--- 7.2.Exercise-Functions and Procedures.sql
+++ 7.2.Exercise-Functions and Procedures.sql
@@ -75,17 +75,35 @@ END
 CREATE FUNCTION ufn_is_word_comprised(set_of_letters varchar(50), word varchar(50))
 RETURNS INT
 BEGIN
-	RETURN word REGEXP (concat('^[',set_of_letters,']+$'));
+	RETURN word REGEXP (CONCAT('^[',set_of_letters,']+$'));
 END
 
 -- 08. Find Full Name
-
+CREATE PROCEDURE usp_get_holders_full_name()
+BEGIN
+	SELECT CONCAT(first_name, ' ', last_name) AS full_name FROM account_holders
+	ORDER BY full_name, id;
+END
 
 -- 9. People with Balance Higher Than
-
+CREATE PROCEDURE usp_get_holders_with_balance_higher_than(balance_number INT)
+BEGIN
+	SELECT a.first_name, a.last_name FROM account_holders AS a
+	JOIN accounts as acc ON a.id = acc.account_holder_id
+	GROUP BY a.id
+	HAVING SUM(acc.balance) > balance_number
+	ORDER BY acc.account_holder_id;
+END 
 
 -- 10. Future Value Function
-
+CREATE FUNCTION ufn_calculate_future_value(
+    sum DECIMAL(19, 4),
+    yearly_interest_rate DOUBLE,
+    num_years INT
+) RETURNS DECIMAL(19, 4)
+BEGIN
+	RETURN sum * POW((1 + yearly_interest_rate), num_years);  
+END
 
 -- 11. Calculating Interest
 
@@ -94,7 +112,21 @@ END
 
 
 -- 13. Withdraw Money
-
+CREATE PROCEDURE usp_withdraw_money(account_id INT, money_amount DECIMAL(19, 4))
+BEGIN
+	IF money_amount > 0 THEN 
+		START TRANSACTION;
+			UPDATE accounts AS a
+			SET a.balance = a.balance - money_amount
+            WHERE account_id = a.id;
+            
+            IF (SELECT balance FROM accounts WHERE account_id = id) < 0 THEN
+				ROLLBACK;
+            ELSE 
+				COMMIT;
+			END IF;
+    END IF;
+END
 
 -- 14. Money Transfer
 
