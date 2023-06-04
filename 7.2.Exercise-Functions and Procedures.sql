59f21fb29eb2283c3482933b91d1490318676ee7
Hristo Yonkov <hristopturs@gmail.com>
2023-06-04 13:28:15 +0300

    Update 7.2.Exercise-Functions and Procedures.sql

--- 7.2.Exercise-Functions and Procedures.sql
+++ 7.2.Exercise-Functions and Procedures.sql
@@ -169,3 +169,44 @@ BEGIN
 END
 
 -- 16. Emails Trigger
+CREATE TABLE `logs`(
+	log_id INT PRIMARY KEY AUTO_INCREMENT,
+	account_id INT NOT NULL,
+    old_sum DECIMAL(19, 4),
+    new_sum DECIMAL(19, 4)
+);
+
+CREATE TRIGGER trigger_balance_update
+AFTER UPDATE ON accounts
+FOR EACH ROW
+BEGIN
+	IF OLD.balance <> NEW.balance
+    THEN
+		INSERT INTO logs (account_id, old_sum, new_sum)
+		VALUE(OLD.id, OLD.balance, NEW.balance);
+    END IF;
+END;
+-- Actual solution
+CREATE TABLE notification_emails(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	recipient INT NOT NULL,
+	subject VARCHAR(100) NOT NULL,
+    body TEXT NOT NULL
+);
+
+CREATE TRIGGER tr_notification_email_creation
+AFTER INSERT ON `logs`
+FOR EACH ROW
+BEGIN
+	INSERT INTO notification_emails(recipient, subject, body)
+    VALUES (
+		NEW.account_id,
+        CONCAT('Balance change for account: ', NEW.account_id),
+        CONCAT_WS(' ',
+			'On',
+            DATE_FORMAT(NOW(), '%b %d %y at %r'),
+            'your balance was changed from',
+            NEW.old_sum,
+            'to',
+            NEW.new_sum));
+END
