e4a660cd1bbc135bb767400f1b12e3de1a9a8a62
Hristo Yonkov <hristopturs@gmail.com>
2023-06-05 15:36:04 +0300

    Update Database Basics MySQL Exam - 13 February 2022.sql

--- solved_exams/Database Basics MySQL Exam - 13 February 2022.sql
+++ solved_exams/Database Basics MySQL Exam - 13 February 2022.sql
@@ -9,3 +9,11 @@
 -- 09. Best categories
 -- 10. Extract clients cards count
 -- 11. Reduce price
+CREATE PROCEDURE udp_reduce_price(category_name VARCHAR(50))
+BEGIN
+	UPDATE products AS p
+    JOIN categories AS c ON p.category_id = c.id
+	JOIN reviews AS r ON r.id = p.review_id
+    SET p.price = price * 0.70
+	WHERE rating < 4 AND c.name = category_name;
+END 
