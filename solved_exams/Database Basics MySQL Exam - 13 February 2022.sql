7abc3691c6ccaeb1e6c7a6658950491d7443fa3a
Hristo Yonkov <hristopturs@gmail.com>
2023-06-05 15:37:30 +0300

    Update Database Basics MySQL Exam - 13 February 2022.sql

--- solved_exams/Database Basics MySQL Exam - 13 February 2022.sql
+++ solved_exams/Database Basics MySQL Exam - 13 February 2022.sql
@@ -8,6 +8,20 @@
 -- 08. First customers
 -- 09. Best categories
 -- 10. Extract clients cards count
+CREATE FUNCTION udf_customer_products_count(name VARCHAR(30))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+	RETURN (
+		SELECT COUNT(p.id) AS total_products
+		FROM customers AS c
+		JOIN orders AS o ON o.customer_id = c.id
+		JOIN orders_products AS op ON op.order_id = o.id
+		JOIN products AS p ON op.product_id = p.id
+		WHERE first_name = `name`
+);
+END
+
 -- 11. Reduce price
 CREATE PROCEDURE udp_reduce_price(category_name VARCHAR(50))
 BEGIN
