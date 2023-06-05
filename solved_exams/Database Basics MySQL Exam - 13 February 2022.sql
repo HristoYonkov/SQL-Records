9b9d763bc9028865775881f3ea8939e4a72826d0
Hristo Yonkov <hristopturs@gmail.com>
2023-06-05 15:38:57 +0300

    Update Database Basics MySQL Exam - 13 February 2022.sql

--- solved_exams/Database Basics MySQL Exam - 13 February 2022.sql
+++ solved_exams/Database Basics MySQL Exam - 13 February 2022.sql
@@ -6,7 +6,19 @@
 -- 06. Quantity
 -- 07. Review
 -- 08. First customers
+SELECT CONCAT(c.first_name, ' ', c.last_name) AS full_name, c.address, o.order_datetime AS order_date
+FROM customers AS c
+JOIN orders as o ON o.customer_id = c.id
+WHERE o.order_datetime <= '2018-12-31' 
+ORDER BY full_name DESC;
+
 -- 09. Best categories
+SELECT COUNT(c.id) AS items_count, c.name, SUM(p.quantity_in_stock) AS total_quantity
+from categories AS c
+JOIN products AS p ON p.category_id = c.id
+GROUP BY c.name
+ORDER BY items_count DESC, total_quantity LIMIT 5;
+
 -- 10. Extract clients cards count
 CREATE FUNCTION udf_customer_products_count(name VARCHAR(30))
 RETURNS INT
